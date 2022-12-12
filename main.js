bde1babb56a7bc985e873f72702bc1e0293000d6
Shane <mhurtado1@berklee.edu>
2022-12-12 10:28:14 -0600

    algorithm yo

---
+++ main.js
@@ -0,0 +1,9 @@
+let myApples = 5
+let yourApples = 11
+
+myApples = myApples + yourApples
+yourApples = myApples - yourApples
+myApples = myApples - yourApples
+
+console.log(myApples)
+console.log(yourApples)
